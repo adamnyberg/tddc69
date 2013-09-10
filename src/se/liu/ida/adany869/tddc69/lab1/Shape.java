10a7ab07c9f6b9022a8ccb185e8dec41f98d2222
Adam Nyberg <adam.nyberg@gmail.com>
2013-09-10 08:49:05 +0200

    removed trash

--- src/se/liu/ida/adany869/tddc69/lab1/Shape.java
+++
@@ -1,9 +0,0 @@
-package se.liu.ida.adany869.tddc69.lab1;
-
-public abstract class Shape {
-    public Shape() {
-        System.out.println("A shape is constructed");
-    }
-
-    public abstract void draw();
-}
