f654eb9f050ae10b235863e3e2b62c53413dfb8a
Harald Pettersson <harpe493@student.liu.se>
2013-09-16 13:24:35 +0200

    Added rotation for poly

--- lab2/src/se/liu/ida/adany869/tddc69/lab2/Poly.java
+++ lab2/src/se/liu/ida/adany869/tddc69/lab2/Poly.java
@@ -9,25 +9,22 @@ public class Poly {
     }
 
     public void rotate(boolean rotateRight){
-        SquareType[][] newArray = new SquareType[PolyArray.length][PolyArray[0].length];
+        int dimension = PolyArray.length;
+        int maxIndex = dimension-1;
+        SquareType[][] newArray = new SquareType[dimension][dimension];
         if (rotateRight) {
-            if (PolyArray.length = 4){
-            newArray[0][0] = PolyArray[3][0];
-            newArray[0][1] = PolyArray[2][0];
-            newArray[0][2] = PolyArray[1][0];
-            newArray[0][3] = PolyArray[0][0];
-            newArray[1][0] = PolyArray[3][1];
-            newArray[1][1] = PolyArray[2][1];
-            newArray[1][2] = PolyArray[2][2];
-            newArray[1][3] = PolyArray[0][1];
-            newArray[2][0] = PolyArray[0][4];
-            newArray[2][1] = PolyArray[0][3];
-            newArray[2][2] = PolyArray[][];
-            newArray[2][3] = PolyArray[][];
-            newArray[3][0] = PolyArray[0][4];
-            newArray[3][1] = PolyArray[0][3];
-            newArray[3][2] = PolyArray[][];
-            newArray[3][3] = PolyArray[][];
+            for (int i=0; i<dimension; i++) {
+                for (int j=0; j<dimension; j++){
+                    newArray[i][j] = PolyArray[maxIndex-j][i];
+                }
+            }
+
+        }
+        else {
+            for (int i=0; i<dimension; i++) {
+                for (int j=0; j<dimension; j++){
+                    newArray[maxIndex-j][i] = PolyArray[i][j];
+                }
             }
         }
     }
