2cd10e423b578d916baa8ede89206a915df5b6dd
Harald Pettersson <harpe493@student.liu.se>
2013-09-18 09:16:56 +0200

    some random shit

---
+++ lab1/src/se/liu/ida/adany869/tddc69/lab1/TetrisComponent.java
@@ -0,0 +1,11 @@
+package se.liu.ida.adany869.tddc69.lab1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Harald
+ * Date: 2013-09-17
+ * Time: 12:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class TetrisComponent {
+}
