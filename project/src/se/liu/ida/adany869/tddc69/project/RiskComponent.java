aa14107f931e37aab99359f71ed8408fadc828db
Adam Nyberg <adam.nyberg@gmail.com>
2013-09-23 14:41:06 +0200

    changed background

--- project/src/se/liu/ida/adany869/tddc69/project/RiskComponent.java
+++ project/src/se/liu/ida/adany869/tddc69/project/RiskComponent.java
@@ -25,7 +25,7 @@ public class RiskComponent extends JComponent{
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.red);
+        g2.setColor(Color.black);
         g2.fillRect(0,0, width, height);
     }
 }
